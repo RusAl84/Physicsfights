5a149a9c9b8d7a0db1eb3b8fc50202a8e24926a3
Aleksey <rusal@bk.ru>
2022-02-15 15:52:05 +0300

    init

---
+++ getTask.py
@@ -0,0 +1,17 @@
+﻿import json
+
+def getTask(id=1):
+    fileObject = open("tasks.json", "r", encoding="UTF-8")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    str1 = aList[id]['task']
+    ans=aList[id]['ans']
+    task=""
+    with open(str1, "r", encoding="UTF-8") as file:
+        task = file.read()
+    return task, ans
+
+if __name__ == '__main__':
+    task, ans = getTask(3)
+    print(task)
+    print(ans)
